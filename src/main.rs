2848e27301a777ea5ccaf3078700b60b8a328b39
eddyxorb <hans-peter.gmuerr@freenet.de>
2021-03-20 20:44:01 +0100

    Implemented complex number with + overload

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::ops::Add;
+
+struct Cx {
+    r: i64,
+    i: i64,
+}
+
+impl Add for Cx {
+    type Output = Cx;
+
+    fn add(self, rhs: Cx) -> Self::Output {
+        Cx {
+            r: self.r + rhs.r,
+            i: self.i + rhs.i,
+        }
+    }
+}
+
+impl Cx {
+    fn new(r: i64, i: i64) -> Self {
+        Self { r, i }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
