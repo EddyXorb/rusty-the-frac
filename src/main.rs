c1e991dccc8aea2958728d23fda7da7aa5e6761f
eddyxorb <hans-peter.gmuerr@freenet.de>
2021-03-22 01:29:44 +0100

    Basic connection between widget and main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod complex;
 mod mandelbrottest;
+mod widget;
 
 fn main() {
+    widget::start_widget();
+
     let a = complex::Cx::new(1.0, 1.0);
     let b = complex::Cx::new(0.0, 10.0);
     let c = a * b;
---
+++ src/widget.rs
@@ -0,0 +1,104 @@
+use druid::kurbo::BezPath;
+use druid::piet::{FontFamily, ImageFormat, InterpolationMode, Text, TextLayoutBuilder};
+use druid::widget::prelude::*;
+use druid::{
+    Affine, AppLauncher, Color, FontDescriptor, LocalizedString, Point, Rect, TextLayout,
+    WindowDesc,
+};
+
+struct CustomWidget;
+
+// If this widget has any child widgets it should call its event, update and layout
+// (and lifecycle) methods as well to make sure it works. Some things can be filtered,
+// but a general rule is to just pass it through unless you really know you don't want it.
+impl Widget<String> for CustomWidget {
+    fn event(&mut self, _ctx: &mut EventCtx, _event: &Event, _data: &mut String, _env: &Env) {}
+
+    fn lifecycle(
+        &mut self,
+        _ctx: &mut LifeCycleCtx,
+        _event: &LifeCycle,
+        _data: &String,
+        _env: &Env,
+    ) {
+    }
+
+    fn update(&mut self, _ctx: &mut UpdateCtx, _old_data: &String, _data: &String, _env: &Env) {}
+
+    fn layout(
+        &mut self,
+        _layout_ctx: &mut LayoutCtx,
+        bc: &BoxConstraints,
+        _data: &String,
+        _env: &Env,
+    ) -> Size {
+        // BoxConstraints are passed by the parent widget.
+        // This method can return any Size within those constraints:
+        // bc.constrain(my_size)
+        //
+        // To check if a dimension is infinite or not (e.g. scrolling):
+        // bc.is_width_bounded() / bc.is_height_bounded()
+        //
+        // bx.max() returns the maximum size of the widget. Be careful
+        // using this, since always make sure the widget is bounded.
+        // If bx.max() is used in a scrolling widget things will probably
+        // not work correctly.
+        if bc.is_width_bounded() | bc.is_height_bounded() {
+            let size = Size::new(100.0, 100.0);
+            bc.constrain(size)
+        } else {
+            bc.max()
+        }
+    }
+
+    // The paint method gets called last, after an event flow.
+    // It goes event -> update -> layout -> paint, and each method can influence the next.
+    // Basically, anything that changes the appearance of a widget causes a paint.
+    fn paint(&mut self, ctx: &mut PaintCtx, data: &String, env: &Env) {
+        // Clear the whole widget with the color of your choice
+        // (ctx.size() returns the size of the layout rect we're painting in)
+        // Note: ctx also has a `clear` method, but that clears the whole context,
+        // and we only want to clear this widget's area.
+        let size = ctx.size();
+        // let rect = size.to_rect();
+        // ctx.fill(rect, &Color::WHITE);
+
+        // Let's burn some CPU to make a (partially transparent) image buffer
+        let image_data = make_image_data(512, 256);
+        let image = ctx
+            .make_image(512, 256, &image_data, ImageFormat::RgbaSeparate)
+            .unwrap();
+        // The image is automatically scaled to fit the rect you pass to draw_image
+        ctx.draw_image(&image, size.to_rect(), InterpolationMode::Bilinear);
+    }
+
+    fn id(&self) -> Option<WidgetId> {
+        None
+    }
+
+    fn type_name(&self) -> &'static str {
+        std::any::type_name::<Self>()
+    }
+}
+
+pub fn start_widget() {
+    //mandelbrot: Vec<u8>, width: usize, height: usize) {
+    let window = WindowDesc::new(|| CustomWidget).title(LocalizedString::new("Mandelbrot-Set"));
+    AppLauncher::with_window(window)
+        .launch("Druid + Piet".to_string())
+        .expect("launch failed");
+}
+
+fn make_image_data(width: usize, height: usize) -> Vec<u8> {
+    let mut result = vec![0; width * height * 4];
+    for y in 0..height {
+        for x in 0..width {
+            let ix = (y * width + x) * 4;
+            result[ix] = x as u8;
+            result[ix + 1] = y as u8;
+            result[ix + 2] = !(x as u8);
+            result[ix + 3] = 200;
+        }
+    }
+    result
+}
